5b0545a269877053d1279ca34f1401bcbdc821a1
flexteck <flexteckse@gmail.com>
2024-12-25 21:58:10 +0100

    implementented a search bar using state and props

--- robotfriends/src/App.js
+++ robotfriends/src/App.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import { robots } from "./robot";
-import SearchBox from "./SearchBar";
+import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>RobotFriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: "",
+    };
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLocaleLowerCase()
+        .includes(this.state.searchfield.toLocaleLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1>RobotFriends</h1>
+        <SearchBar searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- robotfriends/src/SearchBar.js
+++ robotfriends/src/SearchBar.js
@@ -1,10 +1,11 @@
-const SearchBar = () => {
+const SearchBar = ({ searchfield, searchChange }) => {
   return (
     <div className="pa2">
       <input
         className="pa3 ba b--green bg-lightest-blue"
         type="search"
         placeholder="search robot"
+        onChange={searchChange}
       />
     </div>
   );
